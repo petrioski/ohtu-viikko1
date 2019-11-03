f20cdd8917da8c7a281e99718f7123315e0224e6
Petri <petri.karhunkoski@gmail.com>
2019-11-03 20:33:19 +0200

    changed main to separate class, adds jacoco exclusion

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -1,15 +1,15 @@
 package ohtu.ohtuvarasto;
 
-import org.junit.*;
+//import org.junit.*;
 import static org.junit.Assert.*;
-import static org.junit.Assume.assumeNoException;
+//import static org.junit.Assume.assumeNoException;
 
-import org.junit.After;
-import org.junit.AfterClass;
+//import org.junit.After;
+//import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
+//import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 public class VarastoTest {
 
